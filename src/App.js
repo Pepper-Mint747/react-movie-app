b93c62caef9539b8ab864b4a4293dc35055ca6b7
Brian Omoga <omogabrian2060@outlook.com>
2021-05-23 21:56:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
 import './App.css';
 import Movie from "./components/Movie";
-import {Button, Form, FormControl, Nav, Navbar} from "react-bootstrap";
 
 
 function App() {
-
-
-
-
     return (
         <div>
             <Movie />
@@ -17,44 +12,6 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-// <Navbar bg="dark" variant="dark" className={"sticky-top"}>
-//     <Navbar.Brand href="#home">Frenetiks Movie App</Navbar.Brand>
-//     <Nav className="mr-auto">
-//     </Nav>
-//     <Form inline z onSubmit={handleOnSubmit} >
-//         <FormControl
-//             type="search"
-//             placeholder="Search..."
-//             className="mr-sm-2"
-//             value={searchTerm}
-//             onChange={handleOnChange}
-//         />
-//         <Button variant="outline-info"  onClick={handleOnSubmit} >Search</Button>
-//     </Form>
-// </Navbar>
-//
-
-
 
 
 
